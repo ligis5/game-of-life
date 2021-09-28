b6605cb774136901edd03a969945db50ba40abd6
lukas noreika <lukasnor5@gmail.com.com>
2021-09-28 22:03:21 +0300

    created cell obj, system that finds neighbours

---
+++ cell.js
@@ -0,0 +1,22 @@
+function Cell(id, width, height) {
+  const cell = document.createElement("div");
+  cell.setAttribute("id", id);
+  cell.setAttribute("class", "cell");
+  cell.style.width = width + "px";
+  cell.style.height = height + "px";
+
+  this.cell = cell;
+
+  this.status = 0;
+
+  this.cellLives = () => {
+    this.status = 1;
+    cell.style.background = "black";
+  };
+  this.cellDies = () => {
+    this.status = 0;
+    cell.style.background = "white";
+  };
+}
+
+export default Cell;
--- script.js
+++ script.js
@@ -1,41 +1,90 @@
+import Cell from "./cell.js";
+
 const mainDiv = document.querySelector(".main");
 
-const rows = 40;
-const columns = 40;
+const rows = 20;
+const columns = 20;
 const total = rows * columns;
 
 const cellWidth = mainDiv.clientWidth / columns - 2;
 const cellHeight = mainDiv.clientHeight / rows - 2;
 
-const numberOfLifeCells = 10;
+const numberOfLifeCells = 40;
 
-const arrayOfEverything = [];
+let arrayOfEverything = [];
 
 for (let i = 0; i < total; i++) {
   arrayOfEverything.push(0);
 }
+let arrayOfLiveCells = [];
 for (let i = 0; i < numberOfLifeCells; i++) {
-  arrayOfEverything[Math.floor(Math.random() * total)] = 1;
+  let anyNum = Math.floor(Math.random() * total);
+  arrayOfEverything[anyNum] = 1;
+  arrayOfLiveCells.push(anyNum);
 }
 
-for (i = 0; i < total; i++) {
-  const cell = document.createElement("div");
-  cell.setAttribute("id", i);
-  cell.setAttribute("class", "cell");
-  mainDiv.appendChild(cell);
-  cell.style.width = cellWidth + "px";
-  cell.style.height = cellHeight + "px";
-  cell.setAttribute("live", arrayOfEverything[i]);
-  if (cell.getAttribute("live") == "1") {
-    cell.style.background = "black";
+const live = (array) => {
+  let nexArray = [];
+  for (let i = 0; i < total; i++) {
+    let cell = new Cell(i, cellWidth, cellHeight);
+    mainDiv.appendChild(cell.cell);
+    if (array[i] == 1) {
+      cell.cellLives();
+    }
+    let leftNeigbour = i % rows > 0 ? i - 1 : NaN;
+    let rightNeigbour = (i + 1) % rows > 0 ? i + 1 : NaN;
+    let aboveNeigbour = i - 20 >= 0 ? i - 20 : NaN;
+    let belowNeigbour = i + 20 <= total ? i + 20 : NaN;
+    let aboveLeftNeigbour = aboveNeigbour % rows > 0 ? aboveNeigbour - 1 : NaN;
+    let aboveRightNeigbour =
+      (aboveNeigbour + 1) % rows > 0 ? aboveNeigbour + 1 : NaN;
+    let belowLeftNeigbour = belowNeigbour % rows > 0 ? belowNeigbour - 1 : NaN;
+    let belowRightNeigbour =
+      (belowNeigbour + 1) % rows > 0 ? belowNeigbour + 1 : NaN;
+
+    let neighbours = [
+      leftNeigbour,
+      rightNeigbour,
+      aboveNeigbour,
+      belowNeigbour,
+      aboveLeftNeigbour,
+      aboveRightNeigbour,
+      belowLeftNeigbour,
+      belowRightNeigbour,
+    ];
+    let amountOfLivingNeighbour = 0;
+
+    neighbours.forEach((n) => {
+      if (!isNaN(n) && n < 400) {
+        if (cell.status == 1) {
+          amountOfLivingNeighbour++;
+        }
+      }
+    });
+    console.log(amountOfLivingNeighbour, cell);
+    // if (amountOfLivingNeighbour < 2 || amountOfLivingNeighbour > 3) {
+    //   cell.cellDies();
+    // }
+    if (cell.status == 0 && amountOfLivingNeighbour == 3) {
+      cell.cellLives();
+      console.log("work");
+    }
+    nexArray.push(cell.status);
   }
-}
+
+  // const time = setTimeout(() => {
+  //   // live(nexArray);
+  //   console.log("hi");
+  // }, 1000);
+};
+
+live(arrayOfEverything);
+
 const autoGrid = () => {
   document.getElementById("main").style.gridTemplateRows = `repeat(
         (${rows}, ${cellHeight}px)`;
   document.getElementById(
     "main"
   ).style.gridTemplateColumns = `repeat(${columns}, ${cellWidth}px)`;
-  console.log(mainDiv);
 };
 autoGrid();
