a94d0edbcdb649c336b47a346fbc4be6acabded9
lukas noreika <lukasnor5@gmail.com.com>
2021-10-17 18:17:50 +0300

    removed gpu.js, changed how neighbours are calculated, now its done inside cell.If cell reaches red 255 it explodes

--- cell.js
+++ cell.js
@@ -1,33 +1,38 @@
-function Cell(id, width, height) {
+function Cell(columns, rows, width, height) {
   const cell = document.createElement("div");
-  cell.setAttribute("id", id);
+  cell.setAttribute("id", rows);
   cell.setAttribute("class", "cell");
   cell.style.width = width + "px";
   cell.style.height = height + "px";
 
   this.cell = cell;
-  this.id = id;
 
+  this.position = { i: columns, j: rows };
   this.status = 0;
   this.red = 0;
   this.green = 0;
   this.blue = 0;
+
+  this.changeStatusOnClick;
   this.cellLives = () => {
     this.status = 1;
+    this.red += this.red < 255 ? 5 : 0;
     cell.style.background = `rgb(${this.red}, 0, 0)`;
   };
   this.cellDies = () => {
     this.status = 0;
+    this.red = 0;
     cell.style.background = "rgb(40, 51, 153)";
   };
-  // this.neighbours = []
+
+  this.neighbours;
   this.cell.addEventListener("click", () => {
     if (this.status == 0) {
       this.cellLives();
-      console.log(cell.status);
+      this.changeStatusOnClick();
     } else {
       this.cellDies();
-      console.log(cell.status);
+      this.changeStatusOnClick();
     }
   });
 }
--- script.js
+++ script.js
@@ -4,28 +4,65 @@ const mainDiv = document.querySelector(".main");
 const buttonPlay = document.querySelector(".play");
 const buttonReset = document.querySelector(".reset");
 
-const rows = 150;
-const columns = 150;
+const rows = 100;
+const columns = 100;
 const total = rows * columns;
 
 const cellWidth = mainDiv.clientWidth / columns - 2;
 const cellHeight = mainDiv.clientHeight / rows - 2;
 
-const numberOfLifeCells = rows * 10;
+const numberOfLifeCells = 0;
 
 let arrayOfEverythingStatus = [];
 let arrayOfCells = [];
+
 //Create array filled with 0 representing dead cells, and create cells.
 for (let i = 0; i < columns; i++) {
   arrayOfEverythingStatus.push([]);
   arrayOfCells.push([]);
   for (let j = 0; j < rows; j++) {
     arrayOfEverythingStatus[i].push(0);
-    let cell = new Cell(j, cellWidth, cellHeight);
+    let cell = new Cell(i, j, cellWidth, cellHeight);
     mainDiv.appendChild(cell.cell);
-    arrayOfCells[i].push(cell);
+    arrayOfCells[i][j] = cell;
+    // if cell is clicked its state will change.
+    cell.changeStatusOnClick = () => {
+      arrayOfEverythingStatus[i][j] =
+        arrayOfEverythingStatus[i][j] == 0 ? 1 : 0;
+    };
   }
 }
+
+const cellNeighbours = () => {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      arrayOfCells[i][j].neighbours = {
+        left: arrayOfCells[i][j > 0 ? j - 1 : j + (rows - 1)],
+        right: arrayOfCells[i][j < rows - 1 ? j + 1 : j - (rows - 1)],
+        top: arrayOfCells[i > 0 ? i - 1 : i + (columns - 1)][j],
+        bottom: arrayOfCells[i < columns - 1 ? i + 1 : i - (columns - 1)][j],
+        topLeft:
+          arrayOfCells[i > 0 ? i - 1 : i + (columns - 1)][
+            j > 0 ? j - 1 : j + (rows - 1)
+          ],
+        topRight:
+          arrayOfCells[i > 0 ? i - 1 : i + (columns - 1)][
+            j < rows - 1 ? j + 1 : j - (rows - 1)
+          ],
+        bottomLeft:
+          arrayOfCells[i < columns - 1 ? i + 1 : i - (columns - 1)][
+            j > 0 ? j - 1 : j + (rows - 1)
+          ],
+        bottomRight:
+          arrayOfCells[i < columns - 1 ? i + 1 : i - (columns - 1)][
+            j < rows - 1 ? j + 1 : j - (rows - 1)
+          ],
+      };
+    }
+  }
+};
+cellNeighbours();
+
 // add some random 1s to array, to make some cells alive.
 for (let i = 0; i < numberOfLifeCells; i++) {
   arrayOfEverythingStatus[Math.floor(Math.random() * columns)][
@@ -33,65 +70,68 @@ for (let i = 0; i < numberOfLifeCells; i++) {
   ] = 1;
 }
 
-const gpu = new GPU();
-
 // change cells current state to what is in array.
 const cellsStatusChange = (array) => {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       arrayOfCells[i][j].status = array[i][j];
-      if (arrayOfCells[i][j].status == 1) arrayOfCells[i][j].cellLives();
+      if (arrayOfCells[i][j].status == 1) {
+        arrayOfCells[i][j].cellLives();
+      }
       if (arrayOfCells[i][j].status == 0) arrayOfCells[i][j].cellDies();
     }
   }
 };
 cellsStatusChange(arrayOfEverythingStatus);
 
-const nextArrayOfEverything = gpu
-  .createKernel(function (array, rows, columns) {
-    let nextValue = 0;
-    for (let i = 0; i < columns; i++) {
-      for (let j = 0; j < rows; j++) {
-        let x = this.thread.x;
-        let y = this.thread.y;
-        let neighbours =
-          array[y][x > 0 ? x - 1 : x + (rows - 1)] +
-          array[y][x < rows - 1 ? x + 1 : x - (rows - 1)] +
-          array[y > 0 ? y - 1 : y + (columns - 1)][x] +
-          array[y < columns - 1 ? y + 1 : y - (columns - 1)][x] +
-          array[y > 0 ? y - 1 : y + (columns - 1)][
-            x > 0 ? x - 1 : x + rows - 1
-          ] +
-          array[y > 0 ? y - 1 : y + (columns - 1)][
-            x < rows - 1 ? x + 1 : x - (rows - 1)
-          ] +
-          array[y < columns - 1 ? y + 1 : y - (columns - 1)][
-            x > 0 ? x - 1 : x + (rows - 1)
-          ] +
-          array[y < columns - 1 ? y + 1 : y - (columns - 1)][
-            x < rows - 1 ? x + 1 : x - (rows - 1)
-          ];
+const nextArrayOfEverything = (array) => {
+  let nextArray = [];
+  for (let i = 0; i < columns; i++) {
+    nextArray.push([]);
+    for (let j = 0; j < rows; j++) {
+      nextArray[i].push(0);
+      let neighbours = [
+        arrayOfCells[i][j].neighbours.left.status +
+          arrayOfCells[i][j].neighbours.right.status +
+          arrayOfCells[i][j].neighbours.top.status +
+          arrayOfCells[i][j].neighbours.bottom.status +
+          arrayOfCells[i][j].neighbours.topLeft.status +
+          arrayOfCells[i][j].neighbours.topRight.status +
+          arrayOfCells[i][j].neighbours.bottomLeft.status +
+          arrayOfCells[i][j].neighbours.bottomRight.status,
+      ];
 
-        if (
-          (neighbours == 2 && array[y][x] == 1) ||
-          (neighbours == 3 && array[y][x] == 1) ||
-          (array[y][x] == 0 && neighbours == 3)
-        ) {
-          nextValue = 1;
-        }
-        if (
-          (neighbours < 2 && array[y][x] == 0) ||
-          (neighbours > 3 && array[y][x] == 0) ||
-          (neighbours < 2 && array[y][x] == 1) ||
-          (neighbours > 3 && array[y][x] == 1)
-        ) {
-          nextValue = 0;
-        }
+      if (
+        (neighbours == 2 && array[i][j] == 1) ||
+        (neighbours == 3 && array[i][j] == 1) ||
+        (array[i][j] == 0 && neighbours == 3)
+      ) {
+        nextArray[i][j] = 1;
       }
+      if (arrayOfCells[i][j].red >= 255) explodingCell(arrayOfCells[i][j]);
     }
-    return nextValue;
-  })
-  .setOutput([rows, columns]);
+  }
+  return nextArray;
+};
+
+// if cells reaches maximum red 255. then it explodes changing state of some neighbours and their neighbours.
+const explodingCell = (cell) => {
+  Object.values(cell.neighbours).forEach((neigbour) => {
+    let num = Math.random();
+    if (neigbour.status == 1) neigbour.cellDies();
+    if (num >= 0.6) {
+      if (neigbour.status == 0) neigbour.cellLives();
+    }
+    Object.values(neigbour.neighbours).forEach((n) => {
+      let num2 = Math.random();
+      if (n.status == 1) n.cellDies();
+      if (num2 >= 0.8) {
+        if (n.status == 0) n.cellLives();
+      }
+    });
+  });
+  cell.cellDies();
+};
 
 const autoGrid = () => {
   document.getElementById("main").style.gridTemplateRows = `repeat(
@@ -102,32 +142,41 @@ const autoGrid = () => {
 };
 autoGrid();
 
-let time;
+let fps = 5;
+let now;
+let then = Date.now();
+let interval = 1000 / fps;
+let delta;
+
+const gameLoop = () => {
+  if (!stop) window.requestAnimationFrame(gameLoop);
+  else window.cancelAnimationFrame(gameLoop);
+
+  now = Date.now();
+  delta = now - then;
+
+  if (delta > interval) {
+    then = now - (delta % interval);
+
+    arrayOfEverythingStatus = nextArrayOfEverything(arrayOfEverythingStatus);
+    cellsStatusChange(arrayOfEverythingStatus);
+  }
+};
+
 let stop = true;
 buttonPlay.addEventListener("click", () => {
   stop = stop == true ? false : true;
   let button = document.getElementById("play");
   let button2 = document.getElementById("reset");
   if (stop) {
-    clearInterval(time);
+    gameLoop();
     button.style.color = "aqua";
     button2.style.color = "aqua";
   }
   if (!stop) {
+    gameLoop();
     button.style.color = "black";
     button2.style.color = "black";
-  }
-  if (!stop) {
-    time = setInterval(() => {
-      // console.time("time");
-      arrayOfEverythingStatus = nextArrayOfEverything(
-        arrayOfEverythingStatus,
-        rows,
-        columns
-      );
-      cellsStatusChange(arrayOfEverythingStatus);
-      // console.timeEnd("time");
-    }, 1000 / 10);
   }
 });
 buttonReset.addEventListener("click", () => {
